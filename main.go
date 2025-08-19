ecb5bada41550bf919b372469ff37d02fd5a132f
Nick2k4L <nicholas.b.lopez@wsu.edu>
2025-08-18 17:05:28 -0700

    Set up basic Project reqs, such as GIN and air. Tested first HTTP method, works as expected!

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	fmt.Println("hello world")
+	app := gin.Default()
+
+	app.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Welcome to my Gin app",
+		})
+	})
+
+	app.Run(":4000")
 
 }
